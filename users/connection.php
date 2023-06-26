21d21bc286f1a001cb4a660965d9e0360a7e3aa5
BNSHS-Bryan <137507928+BNSHS-Bryan@users.noreply.github.com>
2023-06-26 17:10:25 +0800

    Delete users directory

--- users/connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "gradhelper");
-
-    if($conn->connect_error){
-        die("Connection failed".$conn->connect_error);
-    }
-    echo "";
-    
-    ?>
